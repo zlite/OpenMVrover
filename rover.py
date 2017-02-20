325eb94029719b578681453ebc79c8351fef4ef7
Chris Anderson <gca2@earthlink.net>
2017-02-19 17:06:22 -0800

    Moved to a different motor controller with a reversed throttle curve

--- rover.py
+++ rover.py
@@ -6,19 +6,19 @@ import sensor, image, time, pyb
 from pyb import Servo
 s1 = Servo(1) # P7 Motor
 s2 = Servo(2) # P8 Steering
-cruise_speed = 1000 # how fast should the car drive, range from 1000 to 2000
-steering_gain = 800
+cruise_speed = 1484 # how fast should the car drive, range from 1000 to 2000
+steering_gain = 80
 sensor.reset() # Initialize the camera sensor.
 sensor.set_pixformat(sensor.GRAYSCALE) # or sensor.RGB565
 sensor.set_framesize(sensor.QQVGA) # or sensor.QVGA (or others)
 clock = time.clock() # Tracks FPS.
-s1.pulse_width(2000)  # initialize the motor controller with a high signal for a second
-pyb.delay(1000)
-s1.pulse_width(1000) # return motor controller to low/off throttle
+#s1.pulse_width(2000)  # initialize the motor controller with a high signal for a second
+#pyb.delay(1000)
+#s1.pulse_width(1000) # return motor controller to low/off throttle
 while(True):
     clock.tick() # Track elapsed milliseconds between snapshots().
     img = sensor.snapshot() # Take a picture and return the image.
-    img.find_edges(image.EDGE_CANNY, threshold=(50, 80))  # Find edges
+    img.find_edges(image.EDGE_CANNY, threshold=(74, 174))  # Find edges
     lines = img.find_lines(threshold=40) # Find lines.
     counter = 0
     totalslope = 0
@@ -38,4 +38,4 @@ while(True):
             print (steer_angle)
             s1.pulse_width(cruise_speed) # move forward at cruise speed
             s2.pulse_width(1500 + int(steer_angle*steering_gain)) # steer
-            pyb.delay(10)
+            pyb.delay(5)
