824acaa817a8086c193e09a5686e9f079baa35ca
Chris Anderson <gca2@earthlink.net>
2017-04-08 23:43:16 -0700

    Fixed bugs and timing issues in serial code

--- OpenMVArduino/OpenMVArduino.ino
+++ OpenMVArduino/OpenMVArduino.ino
@@ -5,30 +5,26 @@
 #include <Servo.h> //servo library
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(2, 3); // RX, TX
+SoftwareSerial mySerial(2, 5); // RX, TX
 Servo myservoa, myservob; // create servo objects to control servos
 int SteeringPin = 10;  // This corresponds to Servo Output 1
 int MotorPin = 6;  // This correspondes to Servo Output 2
 int CH3 = 5; // Corresponds to Servo Output 3
 int CH4 = 4; // Corresponds to Servo Output 4
 int CH5 = 9; // Corresponds to Servo Outut 5
-
+int steer, motor;
 
 unsigned long time;
 unsigned long lasttime = 0;
 bool LEDState = LOW;
 
-const int BAUD_RATE = 9600;
+const int BAUD_RATE = 14400;
 
 void setup() {
     Serial.begin(BAUD_RATE);
     myservoa.attach(SteeringPin);// attach servo on Output 1 to servo object
     myservob.attach(MotorPin);// attach servo on Output 2 to servo object
-    myservoa.write(90); // test servo
-    delay (15);
-    myservob.write(10); // test servo
-    delay (15);
-    mySerial.begin(9600);
+    mySerial.begin(9600);   // This is for the debug console
     mySerial.println("Hello, world");
     pinMode(LED_BUILTIN, OUTPUT);  // enable LED 
 }
@@ -42,20 +38,23 @@ void loop() {
        }
   while (Serial.available() > 0) {
    // look for the next valid integer in the incoming serial stream:
-    int steer = Serial.parseInt();
-    mySerial.println(steer);
+    int tempsteer = Serial.parseInt();
+//    mySerial.println(steer);   // turn this on for debgging. Warning: it will make the servos pulse
     // do it again:
-    int motor = Serial.parseInt();
-    mySerial.println(motor);
+    int tempmotor = Serial.parseInt();
+//    mySerial.println(motor);   // turn this on for debgging. Warning: it will make the servos pulse
     // look for the newline. That's the end of the commands
     if (Serial.read() == '\n') {
-        myservoa.write(steer); // send values to output
-        myservob.write(motor);
-    LEDState = !LEDState; // reverse the LED state
-    digitalWrite(LED_BUILTIN, LEDState);   // turn on or off the LED to show activity
-    delay (15);
+        steer=tempsteer;
+        motor=tempmotor;
+        LEDState = !LEDState; // reverse the LED state
+        digitalWrite(LED_BUILTIN, LEDState);   // turn on or off the LED to show activity
     }
   }
+  myservoa.write(steer); // send values to output
+  delay (20);
+  myservob.write(motor);
+  delay (20);
 }
 
 
