265326bb7b7da5f268267f8986405f597cfd34c5
Chris Anderson <gca2@earthlink.net>
2017-05-13 12:01:07 -0700

    Updated for yellow lines

--- colorlinefollowing.py
+++ colorlinefollowing.py
@@ -8,6 +8,8 @@
 import sensor, image, pyb, math, time
 from pyb import Servo
 from pyb import LED
+from pyb import UART
+uart = UART(3, 9600)  # no need to go faster than this. Slower = solid comms
 
 # Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
 # The below thresholds track in general red/green things. You may wish to tune them...
@@ -15,20 +17,21 @@ from pyb import LED
 #              (30, 100, -64, -8, -32, 32), # generic_green_thresholds
 #              (0, 15, 0, 40, -80, -20)] # generic_blue_thresholds
 
-threshold_index = 2 # 0 for red, 1 for green, 2 for blue
+threshold_index = 3
+# 0 for red, 1 for green, 2 for blue
 
 thresholds = [(30, 100, 15, 127, 15, 127), # generic_red_thresholds
-              (30, 100, -64, -8, -32, 32), # generic_green_thresholds
-              (0, 100, -128, -10, -128, 51)] # generic_blue_thresholds
-# You may pass up to 16 thresholds above. However, it's not really possible to segment any
+              (0, 83, -128, 15, -128, 127), # generic_green_thresholds
+              (0, 100, -128, -10, -128, 51), # generic_blue_thresholds
+              (0, 100, -47, 127, 14, 127)] # generic yellow threshold
+              # You may pass up to 16 thresholds above. However, it's not really possible to segment any
 # scene with 16 thresholds before color thresholds start to overlap heavily.
 
-s1 = Servo(1) # P7 Motor
-s2 = Servo(2) # P8 Steering
-print (s1.calibration()) # show throttle servo calibration
+
 cruise_speed = 0 # how fast should the car drive, range from 1000 to 2000
-steering_gain = 400
-steering_direction = -1
+steering_direction = -1   # use this to revers the steering if your car goes in the wrong direction
+steering_gain = 1.1  # calibration for your car's steering sensitivity
+steering_center = 80  # set to your car servo's center point
 kp = 0.4   # P term of the PID
 ki = 0.0     # I term of the PID
 kd = 0.3     # D term of the PID
@@ -65,9 +68,16 @@ def constrain(value, min, max):
         return value
 
 def steer(angle):
-    global steering_gain
-    s1.angle(angle) # steer
-    pyb.delay(10)
+    global steering_gain, cruise_speed, steering_center
+    angle = int(round((angle+steering_center)*steering_gain))
+    constrain(angle, 0, 180)
+    ch1 = str(angle)  # must convert to text to send via Serial
+    ch2 = str(cruise_speed)  # send throttle data, too
+    print(angle)
+    uart.write(ch1)   # send to the Arduino. It looks for channel outputs in order, seperated by a "," and ended with a "\n"
+    uart.write(",")
+    uart.write(ch2)
+    uart.write("\n")
 
 def update_pid():
     global old_time, old_error, measured_angle, set_angle
@@ -93,10 +103,11 @@ def update_pid():
 # will then be averaged with different weights where the most weight is assigned
 # to the roi near the bottom of the image and less to the next roi and so on.
 ROIS = [ # [ROI, weight]
-        (0, 100, 160, 20, 0.7), # You'll need to tweak the weights for your app
+        (0, 100, 160, 20, 0.1), # You'll need to tweak the weights for your app
         (0, 050, 160, 20, 0.3), # depending on how your robot is setup.
-        (0, 000, 160, 20, 0.1)
+        (0, 000, 160, 20, 0.7)
        ]
+
 
 # Compute the weight divisor (we're computing this so you don't have to make weights add to 1).
 weight_sum = 0
